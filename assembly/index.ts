c6aaf25d3fc45ce6f3fadeac3438234b13185d92
Alexandre Capt <acapt@adobe.com>
2021-06-04 12:56:14 +0200

    fix: attempt to fix string vs obj issue

--- assembly/index.ts
+++ assembly/index.ts
@@ -17,7 +17,8 @@ function main(req: Request): Response {
   if (body != null && body.isObj) {
     let obj = body as JSON.Obj;
 
-    let cwv = obj.getObj("cwv");
+    let cwv : JSON.Obj | null = obj.getObj("cwv");
+
     if (!cwv) {
       cwv = new JSON.Obj();
     }
