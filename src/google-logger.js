3d77c9eb2f117e0e36f8a8258738d3daf74b0583
Lars Trieloff <trieloff@adobe.com>
2022-04-12 14:46:20 +0200

    feat(google): partition and cluster data by hostname and timestamp

--- src/google-logger.js
+++ src/google-logger.js
@@ -47,7 +47,13 @@ export class GoogleLogger {
       ...json,
     };
 
+    const clusterdata = {
+      ...data,
+      time: now / 1000, // the cluster table uses TIMESTAMP for time, so that it can be partitioned
+      hostname: (new URL(data.url)).hostname, // the cluster table uses hostname for clustering
+    };
+
     this.logger.log(JSON.stringify(data));
-    this.clusterlogger.log(JSON.stringify(data));
+    this.clusterlogger.log(JSON.stringify(clusterdata));
   }
 }
