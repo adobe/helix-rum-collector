84ebab8a15155b0ff48e0e7914c3852e242bfc01
Lars Trieloff <lars@trieloff.net>
2023-05-04 16:17:45 +0200

    refactor(fastly): replace uses of deprecated fastly API with modern imports

--- src/google-logger.js
+++ src/google-logger.js
@@ -9,7 +9,9 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-/* global fastly */
+/// <reference types="@fastly/js-compute" />
+import { Logger } from 'fastly:logger';
+
 export class GoogleLogger {
   constructor(req) {
     this.subsystemName = 'undefined';
@@ -22,7 +24,7 @@ export class GoogleLogger {
     }
     this.start = Math.floor(Date.now());
     this.req = req;
-    this.clusterlogger = fastly.getLogger('BigQuery-Clustered');
+    this.clusterlogger = new Logger('BigQuery-Clustered');
   }
 
   logRUM(json, id, weight, referer, generation, checkpoint, target, source) {
