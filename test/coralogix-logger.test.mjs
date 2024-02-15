6af0890bfa2edf881f79a9820a596e8e1375b299
Lars Trieloff <lars@trieloff.net>
2024-02-15 10:33:07 +0100

    test(coralogix): expect new log format in tests

--- test/coralogix-logger.test.mjs
+++ test/coralogix-logger.test.mjs
@@ -44,19 +44,24 @@ describe('Test Coralogix Logger', () => {
     assert.equal('helix-rum-collector', logged.applicationName);
     assert.equal('www.foo.com', logged.subsystemName);
     assert.equal(3, logged.severity);
-    assert.equal('http://www.foo.com/testing123', logged.json.edgecompute.url);
-    assert.equal('http://www.foo.com/testing123', logged.json.cdn.url);
-    assert.equal(logged.timestamp, logged.json.time.start_msec);
-    assert(logged.json.time.elapsed < 100);
-    assert.equal('123', logged.json.request.id);
-    assert.equal('GET', logged.json.request.method);
-    assert.equal('bot', logged.json.request.user_agent);
-    assert.equal(42, logged.json.rum.generation);
-    assert.equal(12345, logged.json.rum.checkpoint);
-    assert.equal('http://www.foo.com/testing123', logged.json.rum.target);
-    assert.equal('http://www.foo.com/somesource', logged.json.rum.source);
-    assert.equal(3, logged.json.rum.weight);
-    assert.equal('bar', logged.json.rum.foo);
-    assert.equal(777, logged.json.rum.zoo);
+
+    assert.ok(!logged.json, 'JSON should be empty');
+    assert.ok(logged.text);
+    const loggedJSON = JSON.parse(logged.text);
+
+    assert.equal('http://www.foo.com/testing123', loggedJSON.edgecompute.url);
+    assert.equal('http://www.foo.com/testing123', loggedJSON.cdn.url);
+    assert.equal(logged.timestamp, loggedJSON.time.start_msec);
+    assert(loggedJSON.time.elapsed < 100);
+    assert.equal('123', loggedJSON.request.id);
+    assert.equal('GET', loggedJSON.request.method);
+    assert.equal('bot', loggedJSON.request.user_agent);
+    assert.equal(42, loggedJSON.rum.generation);
+    assert.equal(12345, loggedJSON.rum.checkpoint);
+    assert.equal('http://www.foo.com/testing123', loggedJSON.rum.target);
+    assert.equal('http://www.foo.com/somesource', loggedJSON.rum.source);
+    assert.equal(3, loggedJSON.rum.weight);
+    assert.equal('bar', loggedJSON.rum.foo);
+    assert.equal(777, loggedJSON.rum.zoo);
   });
 });
