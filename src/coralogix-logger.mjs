df7e161a1b1d9d1ca7e3b9f6ffadd3f71e1ea120
Alexandre Capt <acapt@adobe.com>
2024-04-18 16:28:05 +0200

    feat: enhance Coralogix logs (#304)

--- src/coralogix-logger.mjs
+++ src/coralogix-logger.mjs
@@ -25,13 +25,23 @@ export class CoralogixLogger {
     this.logger = new Logger('Coralogix');
   }
 
-  logRUM(json, id, weight, referer, generation, checkpoint, target, source, timePadding) {
+  logRUM(
+    json,
+    id,
+    weight,
+    referer,
+    generation,
+    checkpoint,
+    target,
+    source,
+    timePadding,
+    now = Date.now(),
+  ) {
     console.log(`logging to Coralogix: ${typeof this.logger}`);
     const maskedNow = getMaskedTime(timePadding);
-    console.log('at least I know the time');
 
     const data = {
-      timestamp: maskedNow,
+      timestamp: now,
       applicationName: 'helix-rum-collector',
       subsystemName: this.subsystemName,
       severity: checkpoint === 'error' ? 5 : 3,
@@ -45,14 +55,12 @@ export class CoralogixLogger {
         },
         time: {
           start_msec: maskedNow,
-          elapsed: Math.floor(Date.now()) - this.start,
+          elapsed: Math.floor(now) - this.start,
         },
         request: {
           id,
           method: this.req.method,
-          user_agent: getMaskedUserAgent(this.req.headers) === 'undefined'
-            ? this.req.headers.get('user-agent')
-            : getMaskedUserAgent(this.req.headers),
+          user_agent: this.req.headers.get('user-agent'),
         },
         rum: {
           generation,
@@ -60,6 +68,7 @@ export class CoralogixLogger {
           target: cleanurl(target),
           source: cleanurl(source),
           weight,
+          user_agent: getMaskedUserAgent(this.req.headers),
           ...json,
         },
       }),
